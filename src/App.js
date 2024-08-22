2aa6fcdffd7055a257a16e3fc81c41455d95851b
aida39 <alma2013mtk@gmail.com>
2024-08-22 16:11:22 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, settodos] = useState(["Todo1","Todo2"]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TodoList todos={todos}/>
+      <input type="text" />
+      <button>タスク追加</button>
+      <button>タスク削除 </button>
+      <div>残りのタスク：０</div>
     </div>
   );
 }
