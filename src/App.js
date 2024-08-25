80b6d365686982c3aa225d2bf36209a79b80cfaf
aida39 <alma2013mtk@gmail.com>
2024-08-25 14:40:09 +0900

    機能追加:Todoの削除

--- src/App.js
+++ src/App.js
@@ -4,30 +4,43 @@ import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name: "Todo1", completed: false },
-    //オブジェクトとして属性と値を記述している
+    // { id: 1, name: "Todo1", completed: false },
+    //オブジェクトとして属性と値を記述できる（ただし固定表示になる）
   ]);
 
   const todoNameRef = useRef();
   //refを指定したタグの要素を取得できる
 
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo) => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
+
   const handleAddTodo = () => {
     //コンソールに入力した値を表示させる
     // console.log(todoNameRef.current.value);
     const name = todoNameRef.current.value
+    if (name === "") return
     setTodos((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }]
     })
-    todoNameRef.current.value =null
+    todoNameRef.current.value = null
+  }
+
+  const handleDeleteTodo = () => {
+    const newTodos = todos.filter((todo) => !todo.completed)
+    setTodos(newTodos)
   }
 
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={todoNameRef} />
-      <button onClick={handleAddTodo}>タスク追加</button>
-      <button>タスク削除 </button>
-      <div>残りのタスク：０</div>
+      <button onClick={handleAddTodo}>タスクの追加</button>
+      <button onClick={handleDeleteTodo}>完了したタスクの削除 </button>
+      <div>残りのタスク：{todos.filter((todo) => !todo.completed).length}</div>
     </>
   );
 }
